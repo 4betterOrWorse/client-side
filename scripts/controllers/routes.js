ff54779025ab36fdea753b90aa11a1c9f116f096
austincmatteson <austin.c.matteson@gmail.com>
2018-03-07 10:03:24 -0800

    page base

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -2,7 +2,7 @@
 
 if(window.location.pathname !== '/') {page.base('/client-side');}
 
-page('/client-side', ctx => app.KC.fetchAll(() => app.restaurantsView.initView(ctx)));
-page('/client-side/business/:id', ctx => app.KC.fetchOne(ctx, app.restaurantsView.initDetail));
+page('/', ctx => app.KC.fetchAll(() => app.restaurantsView.initView(ctx)));
+page('/business/:id', ctx => app.KC.fetchOne(ctx, app.restaurantsView.initDetail));
 
 page();

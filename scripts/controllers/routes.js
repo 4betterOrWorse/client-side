ab1380747b9277ec0705d59f5ae58e909c77c5e7
austincmatteson <austin.c.matteson@gmail.com>
2018-03-07 10:07:02 -0800

    page base not working, will revisit at another time

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,7 +1,5 @@
 'use strict';
 
-if(window.location.pathname !== '/') {page.base('/');}
-
 page('/', ctx => app.KC.fetchAll(() => app.restaurantsView.initView(ctx)));
 page('/business/:id', ctx => app.KC.fetchOne(ctx, app.restaurantsView.initDetail));
 

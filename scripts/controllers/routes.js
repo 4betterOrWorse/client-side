3eb04ba076cd9cdd149a184d8ef56e1b915b4f5e
austincmatteson <austin.c.matteson@gmail.com>
2018-03-07 10:12:35 -0800

    will revisit page base later

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,9 +1,5 @@
 'use strict';
 
-if(window.location.pathname !== '/') {
-  page.base('/client-side');
-}
-
 page('/', ctx => app.KC.fetchAll(() => app.restaurantsView.initView(ctx)));
 page('/business/:id', ctx => app.KC.fetchOne(ctx, app.restaurantsView.initDetail));
 

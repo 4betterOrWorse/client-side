46f14c7201b742fdf75550f1730adcd0d970d1d2
austincmatteson <austin.c.matteson@gmail.com>
2018-03-08 15:51:16 -0800

    removed page base

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,30 +1,28 @@
 'use strict';
 
-page.base('/client-side');
-
-page('/client-side', ctx => {
+page('/', ctx => {
   console.log('here');
   app.Yelp.fetchAll(app.yelpView.results)
 });
 
-page('/client-side/business', ctx => app.KC.fetchAll(() => app.restaurantsView.initView(ctx)));
+page('/business', ctx => app.KC.fetchAll(() => app.restaurantsView.initView(ctx)));
 
-page('/client-side/reviews', ctx => {
+page('/reviews', ctx => {
   console.log('happened');
   app.Review.fetchAll(() => app.reviewView.initReview(ctx))});
 
-page('/client-side/reviews/create', ctx => app.Review.create(ctx));
+page('/reviews/create', ctx => app.Review.create(ctx));
 
-page('/client-side/reviews/update/:review_id', ctx => app.Review.fetchOne(ctx, app.reviewView.initUpdateReview));
+page('/reviews/update/:review_id', ctx => app.Review.fetchOne(ctx, app.reviewView.initUpdateReview));
 
-page('/client-side/reviews/:review_id', ctx => app.Review.fetchOne(() => app.reviewView.initSingleReivew(ctx)));
+page('/reviews/:review_id', ctx => app.Review.fetchOne(() => app.reviewView.initSingleReivew(ctx)));
 
-page('/client-side/reviews/delete/:review_id', ctx => app.Review.fetchOne(ctx, app.reviewView.initDelete));
+page('/reviews/delete/:review_id', ctx => app.Review.fetchOne(ctx, app.reviewView.initDelete));
 
-page('/client-side/:id', ctx => {
+page('/:id', ctx => {
   app.Yelp.fetchOne(ctx, app.yelpView.initDetail);
 });
 
-page('/client-side/business/:id', ctx => app.KC.fetchOne(ctx, app.restaurantsView.initDetail));
+page('/business/:id', ctx => app.KC.fetchOne(ctx, app.restaurantsView.initDetail));
 
 page();

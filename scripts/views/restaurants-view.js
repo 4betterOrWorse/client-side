662ddd7bef02d0092ad5a8f92d088d412e970ddc
Dawn Aldrich <dawnstarr.aldrich@gmail.com>
2018-03-08 19:46:26 -0800

    desktop css and health inspection view

--- scripts/views/restaurants-view.js
+++ scripts/views/restaurants-view.js
@@ -86,7 +86,7 @@ $(document).ready(function() {
         // labels: ["January", "February", "March", "April", "May", "June", "July"],
         labels:restDates,
         datasets: [{
-          label: restName,
+          label: 'Total violation points',
           backgroundColor: 'rgb(255, 99, 132)',
           borderColor: 'rgb(255, 99, 132)',
           // data: [0, 10, 5, 2, 20, 30, 45],
